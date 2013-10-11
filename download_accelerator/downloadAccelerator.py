e1f93c4bfa65da99efc959fd3053d44e47dcea4d
Michael Eagar <mkeagar@gmail.com>
2013-10-11 17:54:25 -0600

    I think Download Accelerator is done.  Going to test now.

--- download_accelerator/downloadAccelerator.py
+++ download_accelerator/downloadAccelerator.py
@@ -1,4 +1,93 @@
 import argparse
 import os
 import requests
+import threading
 
+''' CS 360 - Fall 2013 - Lab 2 - Download Accelerator - will download a specified URL using a specified number of threads, or a default number of threads. '''
+class DownloadAccelerator:
+	def __init__(self):
+		''' Parse command line arguments to get the URL to download and if specified, the number of threads to use to download it.'''
+		self.args = None;
+		self.parse_arguments()
+		
+	def parse_arguments(self):
+		''' Parse arguments, which include -n to get the number of threads to use, and a URL, to get the file to download.'''
+		parser = argparse.ArgumentParser(prog='Download Accelerator', description='A script that will download a file in parallel using mulitple threads if a number is specified.', add_help=True)
+		parser.add_argument('-n', '--threads', type=int, action='store', help='Specify the number of threads to use to download the URL.', default=1)
+		parser.add_argument('URL', type=str, action='store', help='Specify the URL for the file to download', default=None)
+		args = parser.parse_args()
+		self.numThreads = args.threads
+		self.url = args.URL
+		
+	def download(self):
+		''' Download the file specified by the URL using the specified number of threads, or one thread if no number was specified.'''
+		# append "index.html" if needed.
+		message = ""
+		suffix = "/"
+		if self.url.endswith(suffix):
+			self.url += 'index.html'
+		
+		# setup download location and size variables for dividing the data among our threads
+		output = self.url.split('/')[-1].strip()
+		partSize = 0
+		message = "Attempting to download " + output + "from URL: " + self.url + " using " + str(self.numThreads) + " threads."
+		print message
+		r = requests.head(self.url)
+		
+		startByte = 0;
+		partSize = 0;
+		endbyte = 0;
+		
+		if not r.headers['content-length']:
+			self.numThreads = 1
+			startByte = -1
+			endByte = -1
+			partSize = -1
+			message = "Content length not provided, using only 1 thread."
+			print message
+		else:
+			partSize = r.headers['content-length'] / self.numThreads
+			
+		if not startByte == -1:
+			endByte = startByte + partsize
+		
+		# create our thread array with the number of threads specified.
+		threads = []
+		for i in range(0,self.numThreads):
+			d = DownThread(self.url, startByte, endByte)
+			threads.append(d)
+			startByte = endByte
+			endByte = startByte + partSize
+			
+		# start our threads downloading!
+		for t in threads:
+			t.start()
+		
+		f = open(output, 'wb')
+		
+		for t in threads:
+			t.join()
+			f.write(t.content)
+		f.close()
+		
+class DownThread(threading.Thread):
+	def __init__(self, url, index1, index2):
+		self.url = url
+		self.startByte = index1
+		self.endByte = index2
+		self.content = None
+		
+		threading.Thread.__init__(self)
+		self._content_consumed = False
+
+	def run(self):
+		if self.startByte == -1:
+			r = requests.get(self.url, stream=True, headers={'accept-encoding':''})
+		else:
+			r = requestes.get(self.url, stream=True, headers={'accept-encoding':'', 'range':self.startByte + '-' + self.endByte})
+
+		self.content = r.content
+
+if __name__ == '__main__':
+	d = DownloadAccelerator()
+	d.download()
---
+++ download_accelerator/test.py
@@ -0,0 +1,5 @@
+''' test.py '''
+url = 'http://www.google.com/'
+print url
+url += 'index.html'
+print url
