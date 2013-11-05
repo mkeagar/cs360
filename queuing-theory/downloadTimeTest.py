19ba102247ba80975bb4562eef85f215f4f6bc98
Michael Eagar <mkeagar@gmail.com>
2013-11-04 19:38:26 -0700

    Queuing Theory initial commit. And webserver load testing stuff for queuing theory lab.

---
+++ queuing-theory/downloadTimeTest.py
@@ -0,0 +1,19 @@
+import time
+import requests
+
+url = "http://162.219.3.125:8080/index.lighttpd.html"
+file_name = "times_webserver.txt"
+f = open(file_name, 'wb')
+total = 0
+for x in range(1, 101):
+	timestart = time.time()
+	r = requests.get(url)
+	timeend = time.time()
+	currenttime = timeend - timestart
+	total += currenttime
+	f.write(str(currenttime) + "\n")
+
+avg = total/100
+f.write("\n" + str(avg) + "\n")
+f.close()
+  
